b8471b89fdfbd630cdf916ecab6ee7aadeace7d0
Marlon Aviz <aviz.marlon@gmail.com>
2017-03-14 08:20:45 -0300

    first version commit

--- fix_formatting.py
+++ fix_formatting.py
@@ -1,5 +1,26 @@
 ### Novell Glosary Scraping
 ## by Marlon Aviz
 
-import re
-	
+FILE_PATH = "scraped-data_optmized.txt"
+saveTo = "final_data.txt"
+handle = open(FILE_PATH, 'r', encoding="utf-8")
+c = 0
+word = None
+definition = []
+for line in handle:
+	line = line.strip()
+	if c == 1:
+		word = line
+		c = 0
+		continue
+	if line == "######":
+		if len(definition) > 0:
+			definition_text = '. '.join(definition)
+			shandle = open(saveTo, 'a', encoding='utf-8')
+			shandle.write(word + "\t" + definition_text + '\n\n')
+			shandle.close()
+			definition.clear()
+		c = 1
+		continue
+	if line != "":
+		definition.append(line)
